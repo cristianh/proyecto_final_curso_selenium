20683fb129bae5c11e52f748f2521e3cf897f0f8
CristianH <crishernandez08@hotmail.com>
2025-07-03 12:07:36 -0500

    Update: limpiando archivo Constants

--- src/test/java/com/juice/utils/Constants.java
+++ src/test/java/com/juice/utils/Constants.java
@@ -2,10 +2,6 @@ package com.juice.utils;
 
 public class Constants {
 
-    //Produccion
-    //public static String UrlBase = "https://juice-shop.herokuapp.com";
-    //Pruebas
-    public static String UrlBase = "http://localhost:3000";
     public static String URLBaseLogin = UrlBase + "/#/login";
     public static String URLBaseRegisterAddress = UrlBase + "/#/address/create";
 
